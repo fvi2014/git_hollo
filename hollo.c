028ab2aaea3ae6b85721a33ef37f6c201e631061
fvi2014 <fvi2014@list.ru>
2017-09-09 23:14:12 +0300

    hollo.c

--- hollo.c
+++ hollo.c
@@ -4,13 +4,13 @@
 int main (void)
 {
 
-  //puts ("Vvedite temperaturu");
+  puts ("Vvedite temperaturu");
 
   float c, f, k;
   scanf ("%f", &c);
   printf ("tconvert%10.2f%s\n", c, " C");
   f = c*9/5+32;
-  k = c + 273.15; 
+  k = c + 273.15;
   printf ("%10.2f%s\n", f, " F");
   printf ( " % 10.2f%s\n " , k, " K" );
 
