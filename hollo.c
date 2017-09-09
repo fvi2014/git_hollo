799b9b422b3f3cb2d89813caaa7ca02b34af0993
fvi2014 <fvi2014@list.ru>
2017-09-09 22:59:54 +0300

    add hollo.c

---
+++ hollo.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main (void)
+{
+
+  //puts ("Vvedite temperaturu");
+
+  float c, f, k;
+  scanf ("%f", &c);
+  printf ("tconvert%10.2f%s\n", c, " C");
+  f = c*9/5+32;
+  k = c + 273.15; 
+  printf ("%10.2f%s\n", f, " F");
+  printf ( " % 10.2f%s\n " , k, " K" );
+
+  return 0;
+}
